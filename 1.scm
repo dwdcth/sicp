5bf5da764db7cefc2ec9d83f0d471b05114ff614
banshan <1310049536@qq.com>
2021-08-03 15:55:18 +0800

    if

--- 1.scm
+++ 1.scm
@@ -6,4 +6,10 @@
    ( cond ((> x 0) x)
           ((= x 0) 0)
           ((< x 0) (- x))))
-(print (abs -5))          
+(print (abs -5))
+;1.1.6 if
+(define (abs2 x)
+    (if (< x 0 )
+        (- x)
+        x))
+(print (abs2 -5))
